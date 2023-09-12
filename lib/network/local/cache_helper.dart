208bd7eb1675f08ba4dbd3186bdfc208439af14b
noura2tarek <Nourat536@gmail.com>
2023-09-12 17:13:06 +0300

    add comments

--- lib/network/local/cache_helper.dart
+++ lib/network/local/cache_helper.dart
@@ -7,15 +7,7 @@ class CacheHelper {
     sharedPreferences = await SharedPreferences.getInstance();
   }
 
-  static Future<bool> putBoolean(
-      {required String key, required bool value}) async {
-    return await sharedPreferences.setBool(key, value);
-  }
-
-  static bool? getBoolean({required String key}) {
-    return sharedPreferences.getBool(key);
-  }
-
+//save data(set data) with any datatype
   static Future<bool> savaData({
     required String key,
     required dynamic value,
@@ -26,20 +18,40 @@ class CacheHelper {
       return await sharedPreferences.setInt(key, value);
     } else if (value is bool) {
       return await sharedPreferences.setBool(key, value);
-    } else {
+    } else if (value is double){
       return await sharedPreferences.setDouble(key, value);
     }
+    else{
+      return await sharedPreferences.setStringList(key, value);
+    }
   }
-
+//get data with a certain key
   static Object? getData({required String key}) {
     return sharedPreferences.get(key);
   }
 
-  static Future<bool> removeData({required String key}) async{
-   return await sharedPreferences.remove(key);
+//save (set) boolean value
+  static Future<bool> putBoolean(
+      {required String key, required bool value}) async {
+    return await sharedPreferences.setBool(key, value);
   }
+
+//get boolean value
+  static bool? getBoolean({required String key}) {
+    return sharedPreferences.getBool(key);
+  }
+
+  //remove data with a certain key
+  static Future<bool> removeData({required String key}) async{
+    return await sharedPreferences.remove(key);
+  }
+  //check if the shared preferences contains a certain value(key) or not
   static bool checkData({required String key}){
     return  sharedPreferences.containsKey(key);
   }
 
+  //clear all data  (clear all the shared preferences)
+  static Future<bool> removeAllData() async{
+    return await  sharedPreferences.clear();
+  }
 }
